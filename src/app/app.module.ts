3891a32dfbf97b411b38622dcd117ae28d00a61d
meandro02 <rd209422@gmail.com>
2024-10-24 08:33:59 -0600

    prueba  de push ACTION

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { CoreComponentsModule } from './core/components/components.module';
 import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CssStyleClass } from '@fortawesome/fontawesome-svg-core';
